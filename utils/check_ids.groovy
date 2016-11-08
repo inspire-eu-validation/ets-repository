14915b703f358cd161a9c5dea8ed48c2ea416bdf
Jon Herrmann <herrmann@interactive-instruments.de>
2016-11-08 19:08:34 +0100

    Check ID script #14

---
+++ utils/check_ids.groovy
@@ -0,0 +1,44 @@
+// Run with 'groovy check_ids.groovy' in utils directory
+//
+// Todos:
+// - relations (parent,...)
+//
+//
+// @author jonherrmann
+import static groovy.io.FileType.FILES
+
+def xmlSlurper = new XmlSlurper()
+
+def idFileMap = [:]
+def errMsgBuilder = ''<<''
+def lineSep = System.getProperty("line.separator")
+
+new File('../').eachFileRecurse(FILES) { file ->
+    if(file.name.startsWith('ets-') && file.name.endsWith('-bsxets.xml')) {
+        println "Parsing $file"
+        def xml = xmlSlurper.parse(file);
+        def ids = xml.'**'.findAll{ it.@id != "" }.'@id'
+        ids.each { i ->
+            // gpath attribute cast required
+            String id = i
+            println "Checking $id"
+            def duplicateCheck = idFileMap[id]
+            if(duplicateCheck!=null) {
+                def errMesg = "Non-unique ID ${id} found in ${file} which is also used in ${duplicateCheck}"
+                println errMesg
+                errMsgBuilder <<= errMesg + lineSep
+            }else{
+                idFileMap.put(id,file)
+            }
+        }
+    }
+}
+
+if(errMsgBuilder.toString().trim().equals("")) {
+    println "${lineSep}OK"
+    System.exit(0)
+}else{
+    println "${lineSep}Results:"
+    println errMsgBuilder
+    System.exit(1)
+}
